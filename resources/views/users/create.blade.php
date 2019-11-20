46dc116a5c524e33ef3f1e0a3cb5d20b58d1c917
Muhamad Amir <mmuhamadamirzaidi@gmail.com>
2019-11-21 03:55:56 +0800

    Minor User Interface Update

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -22,50 +22,63 @@
                             @csrf
                             
                             <h6 class="heading-small text-muted mb-4">{{ __('User information') }}</h6>
-                            <div class="pl-lg-4">
-                                <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="input-name">{{ __('Name') }}</label>
-                                    <input type="text" name="name" id="input-name" class="form-control form-control-alternative{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder="{{ __('Name') }}" value="{{ old('name') }}" required autofocus>
 
-                                    @if ($errors->has('name'))
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('name') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                                <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="input-email">{{ __('Email') }}</label>
-                                    <input type="email" name="email" id="input-email" class="form-control form-control-alternative{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email') }}" value="{{ old('email') }}" required>
+                            <div class="row">
 
-                                    @if ($errors->has('email'))
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('email') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
+                                    <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }} col-md-6">
+                                            <label class="form-control-label" for="input-name">{{ __('Name') }}</label>
+                                            <input type="text" name="name" id="input-name" class="form-control form-control-alternative{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder="{{ __('Name') }}" value="{{ old('name') }}" required autofocus>
+        
+                                            @if ($errors->has('name'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('name') }}</strong>
+                                                </span>
+                                            @endif
+                                        </div>
+                                        <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }} col-md-6">
+                                            <label class="form-control-label" for="input-email">{{ __('Email') }}</label>
+                                            <input type="email" name="email" id="input-email" class="form-control form-control-alternative{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email') }}" value="{{ old('email') }}" required>
+        
+                                            @if ($errors->has('email'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('email') }}</strong>
+                                                </span>
+                                            @endif
+                                        </div>
 
-                                <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="input-name">{{ __('Phone Number') }}</label>
-                                    <input type="text" name="name" id="input-name" class="form-control form-control-alternative{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder="{{ __('Phone Number') }}" value="{{ old('name') }}" required autofocus>
+                            </div>
 
-                                    @if ($errors->has('name'))
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('name') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                                <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
-                                    <label class="form-control-label" for="input-name">{{ __('Company') }}</label>
-                                    <input type="text" name="name" id="input-name" class="form-control form-control-alternative{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder="{{ __('Company') }}" value="{{ old('name') }}" required autofocus>
+                            <div class="row">
 
-                                    @if ($errors->has('name'))
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('name') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
+                                    <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }} col-md-6">
+                                            <label class="form-control-label" for="input-name">{{ __('Phone Number') }}</label>
+                                            <input type="text" name="name" id="input-name" class="form-control form-control-alternative{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder="{{ __('Phone Number') }}" value="{{ old('name') }}" required autofocus>
+        
+                                            @if ($errors->has('name'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('name') }}</strong>
+                                                </span>
+                                            @endif
+                                        </div>
+                                        <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }} col-md-6">
+                                            <label class="form-control-label" for="input-name">{{ __('Company') }}</label>
+                                            <input type="text" name="name" id="input-name" class="form-control form-control-alternative{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder="{{ __('Company') }}" value="{{ old('name') }}" required autofocus>
+        
+                                            @if ($errors->has('name'))
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $errors->first('name') }}</strong>
+                                                </span>
+                                            @endif
+                                        </div>
 
-                                <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
+                            </div>
+
+                            <div class="row">
+                                
+
+                                
+
+                                <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }} col-md-6">
                                     <label class="form-control-label" for="input-password">{{ __('Password') }}</label>
                                     <input type="password" name="password" id="input-password" class="form-control form-control-alternative{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="{{ __('Password') }}" value="" required>
                                     
@@ -75,14 +88,14 @@
                                         </span>
                                     @endif
                                 </div>
-                                <div class="form-group">
-                                    <label class="form-control-label" for="input-password-confirmation">{{ __('Confirm Password') }}</label>
+                                <div class="form-group col-md-6">
+                                    <label class="form-control-label " for="input-password-confirmation">{{ __('Confirm Password') }}</label>
                                     <input type="password" name="password_confirmation" id="input-password-confirmation" class="form-control form-control-alternative" placeholder="{{ __('Confirm Password') }}" value="" required>
                                 </div>
                             </div>
 
                             <h6 class="heading-small text-muted mb-4">{{ __('Health information') }}</h6>
-                            <div class="pl-lg-4 row">
+                            <div class="row">
 
                                     <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }} col-md-4 " >
                                             <label class="form-control-label" for="input-name">{{ __('Gender') }}</label>
@@ -125,20 +138,20 @@
 
                             </div>
 
-                            <div class="pl-lg-4 row">
+                            <div class="row">
 
                                     <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }} col-md-4 " >
-                                            <label class="form-control-label" for="input-name">{{ __('Gender') }}</label>
+                                            <label class="form-control-label" for="input-name">{{ __('Smoking Preference') }}</label>
                                             {{-- <input type="text" name="name" id="input-name" class="form-control form-control-alternative{{ $errors->has('name') ? ' is-invalid' : '' }}" placeholder="{{ __('Weight') }}" value="{{ old('name') }}" required autofocus> --}}
 
                                             <div class="col-auto row">
                                                 <div class="custom-control custom-radio mb-3 col-md-3">
                                                     <input name="custom-radio-2" class="custom-control-input" id="customRadio3"  checked="" type="radio">
-                                                    <label class="custom-control-label" for="customRadio3">Male</label>
+                                                    <label class="custom-control-label" for="customRadio3">Yes</label>
                                                   </div>
                                                   <div class="custom-control custom-radio mb-3 col-md-3">
                                                     <input name="custom-radio-2" class="custom-control-input" id="customRadio4" type="radio">
-                                                    <label class="custom-control-label" for="customRadio4">Female</label>
+                                                    <label class="custom-control-label" for="customRadio4">No</label>
                                                   </div>
 
                                             </div>
@@ -168,7 +181,7 @@
 
                             </div>
 
-                            <div class="pl-lg-4">
+                            <div>
 
                                 <div class="form-group{{ $errors->has('name') ? ' has-danger' : '' }}">
                                     <label class="form-control-label" for="input-name">{{ __('Family History') }}</label>
