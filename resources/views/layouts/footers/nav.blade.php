cf7603c75560b3050dc38d1ecfe355d805f191ae
Muhamad Amir <mmuhamadamirzaidi@gmail.com>
2019-11-27 19:08:34 +0800

    Remove Footer At Bottom Right Page

--- resources/views/layouts/footers/nav.blade.php
+++ resources/views/layouts/footers/nav.blade.php
@@ -1,29 +1,8 @@
 <div class="row align-items-center justify-content-xl-between">
-    <div class="col-xl-6">
+    <div class="col-xl-12">
         <div class="copyright text-center text-xl-left text-muted">
             &copy; {{ now()->year }} <a href="#" class="font-weight-bold ml-1" target="_blank">Muhamad Amir Bin Zaidi</a>
-            {{-- &amp; --}}
-            {{-- <a href="https://www.updivision.com" class="font-weight-bold ml-1" target="_blank">Updivision</a> --}}
         </div>
-    </div>
-    <div class="col-xl-6">
-        <ul class="nav nav-footer justify-content-center justify-content-xl-end">
-            <li class="nav-item">
-                <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Footer 1</a>
-            </li>
-            <li class="nav-item">
-                <a href="https://www.updivision.com" class="nav-link" target="_blank">Footer 2</a>
-            </li>
-            <li class="nav-item">
-                <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
-            </li>
-            <li class="nav-item">
-                <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
-            </li>
-            <li class="nav-item">
-                <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md" class="nav-link" target="_blank">MIT License</a>
-            </li>
-        </ul>
     </div>
 </div>
 
