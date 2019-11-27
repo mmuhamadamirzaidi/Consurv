a5124199f4ccd2491b639c12bf563f385b102794
Muhamad Amir <mmuhamadamirzaidi@gmail.com>
2019-11-27 21:04:03 +0800

    Update Risk Level Card Percentage & Remove Table Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,6 +22,11 @@
                                     </div>
                                 </div>
                             </div>
+                            
+                            <p class="mt-3 mb-0 text-sm">
+                                <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                                <span class="text-nowrap">Since last month</span>
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -40,6 +45,11 @@
                                     </div>
                                 </div>
                             </div>
+
+                            <p class="mt-3 mb-0 text-sm">
+                                <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                                <span class="text-nowrap">Since last month</span>
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -58,56 +68,28 @@
                                     </div>
                                 </div>
                             </div>
+
+                            <p class="mt-3 mb-0 text-sm">
+                                <span class="text-warning mr-2"><i class="fa fa-arrow-down"></i> 3.48%</span>
+                                <span class="text-nowrap">Since last month</span>
+                            </p>
                         </div>
                     </div>
                 </div>
+
             </div>
         </div>
     </div>
 </div>
 <div class="container-fluid mt--7">
     <div class="row">
-        <div class="col-xl-8 mb-5 mb-xl-0">
-            <div class="card bg-gradient-default shadow">
-                <div class="card-header bg-transparent">
-                    <div class="row align-items-center">
-                        <div class="col">
-                            <h6 class="text-uppercase text-light ls-1 mb-1">Overview</h6>
-                            <h2 class="text-white mb-0">Value</h2>
-                        </div>
-                        <div class="col">
-                            <ul class="nav nav-pills justify-content-end">
-                                <li class="nav-item mr-2 mr-md-0" data-toggle="chart" data-target="#chart-sales" data-update='{"data":{"datasets":[{"data":[0, 20, 10, 30, 15, 40, 20, 60, 60]}]}}' data-prefix="$" data-suffix="k">
-                                    <a href="#" class="nav-link py-2 px-3 active" data-toggle="tab">
-                                        <span class="d-none d-md-block">Month</span>
-                                        <span class="d-md-none">M</span>
-                                    </a>
-                                </li>
-                                <li class="nav-item" data-toggle="chart" data-target="#chart-sales" data-update='{"data":{"datasets":[{"data":[0, 20, 5, 25, 10, 30, 15, 40, 40]}]}}' data-prefix="$" data-suffix="k">
-                                    <a href="#" class="nav-link py-2 px-3" data-toggle="tab">
-                                        <span class="d-none d-md-block">Week</span>
-                                        <span class="d-md-none">W</span>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <!-- Chart -->
-                    <div class="chart">
-                        <!-- Chart wrapper -->
-                        <canvas id="chart-sales" class="chart-canvas"></canvas>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-4">
+
+        <div class="col-xl-12">
             <div class="card shadow">
                 <div class="card-header bg-transparent">
                     <div class="row align-items-center">
                         <div class="col">
-                            <h6 class="text-uppercase text-muted ls-1 mb-1">Performance</h6>
+                            <h6 class="text-uppercase text-muted ls-1 mb-1">Overview</h6>
                             <h2 class="mb-0">Total Rigs</h2>
                         </div>
                     </div>
@@ -121,112 +103,16 @@
             </div>
         </div>
     </div>
+    
     <div class="row mt-5">
-        <div class="col-xl-8 mb-5 mb-xl-0">
+
+        <div class="col-xl-12">
             <div class="card shadow">
                 <div class="card-header border-0">
                     <div class="row align-items-center">
                         <div class="col">
-                            <h3 class="mb-0">Page Visits</h3>
-                        </div>
-                        <div class="col text-right">
-                            <a href="#!" class="btn btn-sm btn-primary">See all</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="table-responsive">
-                    <!-- Projects table -->
-                    <table class="table align-items-center table-flush">
-                        <thead class="thead-light">
-                            <tr>
-                                <th scope="col">Page name</th>
-                                <th scope="col">Visitors</th>
-                                <th scope="col">Unique users</th>
-                                <th scope="col">Bounce rate</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row">
-                                    /consurv/
-                                </th>
-                                <td>
-                                    4,569
-                                </td>
-                                <td>
-                                    340
-                                </td>
-                                <td>
-                                    <i class="fas fa-arrow-up text-success mr-3"></i> 46,53%
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">
-                                    /consurv/index.html
-                                </th>
-                                <td>
-                                    3,985
-                                </td>
-                                <td>
-                                    319
-                                </td>
-                                <td>
-                                    <i class="fas fa-arrow-down text-warning mr-3"></i> 46,53%
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">
-                                    /consurv/charts.html
-                                </th>
-                                <td>
-                                    3,513
-                                </td>
-                                <td>
-                                    294
-                                </td>
-                                <td>
-                                    <i class="fas fa-arrow-down text-warning mr-3"></i> 36,49%
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">
-                                    /consurv/tables.html
-                                </th>
-                                <td>
-                                    2,050
-                                </td>
-                                <td>
-                                    147
-                                </td>
-                                <td>
-                                    <i class="fas fa-arrow-up text-success mr-3"></i> 50,87%
-                                </td>
-                            </tr>
-                            <tr>
-                                <th scope="row">
-                                    /consurv/profile.html
-                                </th>
-                                <td>
-                                    1,795
-                                </td>
-                                <td>
-                                    190
-                                </td>
-                                <td>
-                                    <i class="fas fa-arrow-down text-danger mr-3"></i> 46,53%
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-        <div class="col-xl-4">
-            <div class="card shadow">
-                <div class="card-header border-0">
-                    <div class="row align-items-center">
-                        <div class="col">
-                            <h3 class="mb-0">Rigs Ranking</h3>
+                            <h6 class="text-uppercase text-muted ls-1 mb-1">Overview</h6>
+                            <h2 class="mb-0">Rigs Ranking</h2>
                         </div>
                         <div class="col text-right">
                             <a href="#!" class="btn btn-sm btn-primary">See all</a>
@@ -239,8 +125,9 @@
                         <thead class="thead-light">
                             <tr>
                                 <th scope="col">Rig Name</th>
+                                <th scope="col">Total Patient</th>
                                 <th scope="col">Ranking</th>
-                                <th scope="col">Healthy Rig Status</th>
+                                <th scope="col">Healthy Percentage</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -249,17 +136,13 @@
                                     Rig A
                                 </th>
                                 <td>
-                                    120
+                                    20
                                 </td>
                                 <td>
-                                    <div class="d-flex align-items-center">
-                                        <span class="mr-2">80%</span>
-                                        <div>
-                                            <div class="progress">
-                                                <div class="progress-bar bg-gradient-success" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;"></div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    1/300
+                                </td>
+                                <td>
+                                        80%
                                 </td>
                             </tr>
                             <tr>
@@ -267,17 +150,13 @@
                                     Rig B
                                 </th>
                                 <td>
-                                    100
+                                    10
                                 </td>
                                 <td>
-                                    <div class="d-flex align-items-center">
-                                        <span class="mr-2">70%</span>
-                                        <div>
-                                            <div class="progress">
-                                                <div class="progress-bar bg-gradient-success" role="progressbar" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100" style="width: 70%;"></div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    2/300
+                                </td>
+                                <td>
+                                        70%
                                 </td>
                             </tr>
                             <tr>
@@ -285,35 +164,26 @@
                                     Rig C
                                 </th>
                                 <td>
-                                    80
+                                    8
                                 </td>
                                 <td>
-                                    <div class="d-flex align-items-center">
-                                        <span class="mr-2">80%</span>
-                                        <div>
-                                            <div class="progress">
-                                                <div class="progress-bar bg-gradient-success" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%;"></div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    3/300
                                 </td>
-                            </tr>
+                                <td>
+                                        60%
+                                </td>
                             <tr>
                                 <th scope="row">
                                     Rig D
                                 </th>
                                 <td>
-                                    60
+                                    120
                                 </td>
                                 <td>
-                                    <div class="d-flex align-items-center">
-                                        <span class="mr-2">60%</span>
-                                        <div>
-                                            <div class="progress">
-                                                <div class="progress-bar bg-gradient-warning" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;"></div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    4/300
+                                </td>
+                                <td>
+                                        50%
                                 </td>
                             </tr>
                             <tr>
@@ -321,17 +191,13 @@
                                     Rig E
                                 </th>
                                 <td>
-                                    40
+                                    120
                                 </td>
                                 <td>
-                                    <div class="d-flex align-items-center">
-                                        <span class="mr-2">30%</span>
-                                        <div>
-                                            <div class="progress">
-                                                <div class="progress-bar bg-gradient-danger" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 30%;"></div>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    5/300
+                                </td>
+                                <td>
+                                        40%
                                 </td>
                             </tr>
                         </tbody>
